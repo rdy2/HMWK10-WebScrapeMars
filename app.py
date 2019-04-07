c13d4ab2bcfd668155148643908d48006395c5cb
rdy2 <45048386+rdy2@users.noreply.github.com>
2019-04-07 15:32:50 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
---
+++ mars.py
@@ -0,0 +1,35 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime as dt
+
+
+def scrape_all():
+
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    news_title, news_paragraph = mars_news(browser)
+
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "hemispheres": hemispheres(browser),
+        "weather": twitter_weather(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    browser.quit()
+    return data
+
+
+def mars_news(browser):
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=0.5)
+
+    html = browser.html
+    news_soup = BeautifulSoup(html, "html.parser")
+
+    
